3b340f7ccc6c1df8e688b4f6b16fc52216a3a74c
fromanator <andrew.from@target.com>
2016-06-10 13:25:30 -0500

    Fix checkstyle issues

--- ratpack-core/src/main/java/ratpack/http/client/internal/NonPoolingChannelPool.java
+++ ratpack-core/src/main/java/ratpack/http/client/internal/NonPoolingChannelPool.java
@@ -1,15 +1,29 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ratpack.http.client.internal;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoop;
 import io.netty.channel.pool.ChannelPool;
 import io.netty.channel.pool.ChannelPoolHandler;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.Promise;
-import ratpack.exec.Execution;
 
 /**
  * Creates a channel pool that does no pooling.
@@ -60,6 +74,7 @@ public final class NonPoolingChannelPool implements ChannelPool {
     try {
       handler.channelReleased(channel);
     } catch (Exception e) {
+      // If failed channel closes automatically
     }
     return promise;
   }
